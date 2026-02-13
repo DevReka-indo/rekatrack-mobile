766bac95ea36cd1fa1282b60a6ba1f53946d2119
DevReka-indo <ti.dev.reka@gmail.com>
2026-02-13 10:51:11 +0700

    remove binary splash asset from diff and use existing logo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,14 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
+import { Stack, router, usePathname } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import { BackHandler, Platform } from "react-native";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import "@/utils/locationTask";
+import { useEffect } from "react";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -15,6 +18,24 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (Platform.OS !== "android") return;
+
+    const sub = BackHandler.addEventListener("hardwareBackPress", () => {
+      const isDashboard = pathname === "/(tabs)" || pathname === "/(tabs)/index";
+
+      if (!router.canGoBack() && !isDashboard) {
+        router.replace("/(tabs)");
+        return true;
+      }
+
+      return false;
+    });
+
+    return () => sub.remove();
+  }, [pathname]);
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
@@ -25,7 +46,7 @@ export default function RootLayout() {
           options={{ presentation: "modal", title: "Modal" }}
         />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
     </ThemeProvider>
   );
 }
